f47bb0739b55e5373bd6f9d7abdc64afc0c1dd95
kanizshapla <kanizshapla46@gmail.com>
2024-03-17 12:42:44 +0600

    Add react to vite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
+import react from "@vitejs/plugin-react";
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [react()],
 };
