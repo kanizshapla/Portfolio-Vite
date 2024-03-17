6dc94f3b24c0c6487eb23de55aa7d0ef02d3a98b
kanizshapla <kanizshapla46@gmail.com>
2024-03-17 12:18:23 +0600

    Add build commant to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/Portfolio-Vite/",
   plugins: [react()],
-  // build: {
-  //   OutDir: "build",
-  // },
+  build: {
+    OutDir: "build",
+  },
 });
