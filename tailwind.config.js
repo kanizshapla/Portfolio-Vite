04adfd8d124897ce438bb1c426dd05bb378a3830
kanizshapla <kanizshapla46@gmail.com>
2024-03-17 15:23:54 +0600

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
-import react from "@vitejs/plugin-react";
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [react()],
+  plugins: [],
 };
